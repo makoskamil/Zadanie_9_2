9f0db80b3cb6fd35ceb9101bf9a90db3eae39168
Kamil Makoś <makos.kamil@o2.pl>
2018-03-22 23:59:04 +0100

    add ;

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
 var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
-var allNames = []
+var allNames = [];
 
 var allNames = allNames.concat(femaleNames, maleNames);
 console.log(allNames);
