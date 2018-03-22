5369719e9737d9f31c7545c778f2160905425492
Kamil Makoś <makos.kamil@o2.pl>
2018-03-22 23:54:52 +0100

    add_files

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+var allNames = []
+
+var allNames = allNames.concat(femaleNames, maleNames);
+console.log(allNames);
+
+var allNamesToString = allNames.join(' | ');
+alert(allNames);
+
+var newName = 'Marian'
+
+if (allNames.indexOf(newName) === -1) {
+    var x = allNames.push(newName);
+}
+else {
+    alert("Can't add newName to array");
+}
